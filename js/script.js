cc1b2ff498cae3471793789bd7dc643ec7e4ce28
ononivamij <jonathanononivami@gmail.com>
2023-09-12 12:40:33 +0100

    index.html script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,6 @@
-const main = () => {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    const dateElem = document.querySelector('[data-testid="currentDayOfTheWeek"]')
-    const timeElem = document.querySelector('[data-testid="currentUTCTime"]')
-    const setValue = (elem, val) => {  elem.innerHTML = val }
-    setValue(dateElem, days[new Date().getUTCDay()])
-    window.setInterval(()=> {
-        setValue(dateElem, days[new Date().getUTCDay()])
-    }, 60000)
-    const timer = () => {
-        window.setInterval(() => {
-            setValue(timeElem, Date.now())
-        }, 100)
-    }
-    timer()
-}
-window.addEventListener('DOMContentLoaded', (event) => { main()});
+const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const day= new Date();
+let dayOfTheWeek = weekday[day.getDay()]
+let date = day.toLocaleString ()
+document.querySelector('h3').innerHTML = ('Hello!' + ' ' + "It's" + ' ' + dayOfTheWeek)
+document.querySelector('h4').innerHTML = 'The time is' + ' ' +  date
